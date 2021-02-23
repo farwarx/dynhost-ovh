24c3cd720a5d405970d5f5d0dab0b696b845bdfd
Farwarx <farwarx@gmail.com>
2021-02-23 14:17:16 +0100

    change dig by curl & log mode

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # dynhost-ovh
 A simple (cron) script to update DynHost on OVH hosting
+Fork of: https://github.com/yjajkiew/dynhost-ovh
+Link: https://yann.me/dynhost-ovh-pour-raspberry-pi-ou-une-machine-linux/
 
 # Prerequisites
-This script works with two linux commands : curl and dig.
-If you do not have the dig you must install it from the package "dnsutils" :
-```sh
-sudo apt-get update
-sudo apt-get install dnsutils
-```
+This script works with two linux commands : curl & host
 
 # How to use
 1. Download the dynhost.sh script and put it in the folder /etc/cron.hourly (to check every hour)
@@ -15,7 +12,6 @@ sudo apt-get install dnsutils
 3. Modify the script with variables : HOST, LOGIN, PASSWORD
 
 # How it works
-1. The command dig is used to retrieve the IP address of your domain name.
+1. The command host is used to retrieve the IP address of your domain name.
 2. The command curl (with the website ifconfig.co) is used to retrieve the current public IP address of your machine.
 3. The two IPs are compared and if necessary a curl command to OVH is used to update your DynHost with your current public IP address.
-
--- dynhost.sh
+++ dynhost.sh
@@ -1,20 +1,23 @@
-#!/usr/bin/env sh
+#/bin/sh
 
-# Account configuration
-HOST=DOMAINE_NAME
-LOGIN=LOGIN
-PASSWORD=PASSWORD
-
-PATH_LOG=/var/log/dynhostovh.log
+### CONFIG ###
+HOST="DOMAIN_NAME"
+LOGIN="LOGIN"
+PASSWORD="PASSWORD"
+LOG_FILE=/var/log/dynhost.log
 
 # Get current IPv4 and corresponding configured
-HOST_IP=$(dig +short $HOST A)
+HOST_IP=$(host -4 ${HOST} | cut -d" " -f4)
 CURRENT_IP=$(curl -m 5 -4 ifconfig.co 2>/dev/null)
-if [ -z $CURRENT_IP ]
+
+### EXECUTION ###
+touch "$LOG_FILE"
+LOG_PREFIX="$(date) - Current IP: $CURRENT_IP ; Host IP: $HOST_IP"
+
+if [ -z $CURRENT_IP ] || [ -z $HOST_IP ]
 then
-  CURRENT_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-fi
-CURRENT_DATETIME=$(date -R)
+    echo "No IP retrieved" >> $PATH_LOG
+else
 
 # Update dynamic IPv4, if needed
 if [ -z $CURRENT_IP ] || [ -z $HOST_IP ]
@@ -23,7 +26,10 @@ then
 else
   if [ "$HOST_IP" != "$CURRENT_IP" ]
   then
-    RES=$(curl -m 5 --user "$LOGIN:$PASSWORD" "https://www.ovh.com/nic/update?system=dyndns&hostname=$HOST&myip=$CURRENT_IP")
-    echo "[$CURRENT_DATETIME]: IPv4 has changed - request to OVH DynHost: $RES" >> $PATH_LOG
+    RES=$(curl -m 5 --insecure --user "$LOGIN:$PASSWORD" "https://www.ovh.com/nic/update?system=dyndns&hostname=$HOST&myip=$CURRENT_IP")
+    LOG_SUFFIX="IP has changed - Result request dynHost: $RES"
+  else
+    LOG_SUFFIX="IP has not changed"
   fi
 fi
+echo "$LOG_PREFIX - $LOG_SUFFIX" >> "$LOG_FILE"
